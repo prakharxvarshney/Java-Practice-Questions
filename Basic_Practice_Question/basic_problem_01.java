d0fa6f3606f02a4be799f1172ce62101114796e6
prakharxvarshney <prakhar.varshney200004@gmail.com>
2026-02-09 19:34:44 +0530

    Pushing - Problem 1 and its solution of basic Prob

---
+++ Basic_Practice_Question/basic_problem_01.java
@@ -0,0 +1,10 @@
+package Basic_Practice_Question;
+
+// Write a Java program to print 'Hello' on screen and your name on a separate line.
+
+public class basic_problem_01 {
+    public static void main(String[] args) {
+        System.out.print("Hello\nPrakhar Varshney");
+    }
+    
+}

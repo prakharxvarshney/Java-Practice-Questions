47b52a3bcf3a26d476ac6aae2e77e1ac4575a562
prakharxvarshney <prakhar.varshney200004@gmail.com>
2026-02-09 19:41:47 +0530

    Pushing-Basic_Practice_Question 2 and 3.

---
+++ Basic_Practice_Question/basic_problem_02.java
@@ -0,0 +1,9 @@
+package Basic_Practice_Question;
+//Write a Java program to print the sum of two numbers.
+public class basic_problem_02 {
+    public static void main(String[] args) {
+        int num1 = 20, num2 = 30;
+        int sum = num1+num2;
+        System.out.println(sum);
+    }
+}
---
+++ Basic_Practice_Question/basic_problem_03.java
@@ -0,0 +1,11 @@
+package Basic_Practice_Question;
+//Write a Java program to divide two numbers and print them on the screen.
+public class basic_problem_03 {
+
+    public static void main(String[] args) {
+        int num1 = 120, num2 = 30;
+        int sum = num1/num2;
+        System.out.println(sum);
+    }
+    
+}

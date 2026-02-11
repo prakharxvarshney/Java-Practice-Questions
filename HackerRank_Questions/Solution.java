ae7b73423ec91479282f0b415b0a48732fe34016
prakharxvarshney <prakhar.varshney200004@gmail.com>
2026-02-11 18:44:39 +0530

    HackerRank Introduct Java Loop II

---
+++ HackerRank_Questions/Solution.java
@@ -0,0 +1,35 @@
+package HackerRank_Questions;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = scanner.nextInt();
+        int[] a = new int[counter];
+        int[] b = new int[counter];
+        int[] n = new int[counter];
+
+        for (int i = 0; i<counter; i++) {
+            a[i] = scanner.nextInt();
+            b[i] = scanner.nextInt();
+            n[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i<counter; i++) {
+            int sum = a[i];
+            int power = 1;
+            for (int j=0; j<n[i]; j++) {
+                sum = sum + b[i]*power;
+                power = power*2;
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
